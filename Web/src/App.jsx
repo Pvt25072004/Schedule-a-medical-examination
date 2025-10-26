643c6695daea0b76a878e9ecac2a6d9c7194df76
TranDucLuong1404 <ducluongdz48@gmail.com>
2025-10-26 22:11:19 +0700

    build STL Clinic website

---
+++ Web/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import STLClinicWebsite from './components/STLClinicWebsite';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <STLClinicWebsite />
+    </div>
+  );
+}
+
+export default App;
